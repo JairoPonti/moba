5dc76addb05033a14fec6853c788fe65225bf4d1
facu98 <facuortiz.rg@gmail.com>
2021-02-22 03:12:41 -0300

    test

--- api/server.js
+++ api/server.js
@@ -2,13 +2,13 @@ const path = require("path");
 const morgan = require("morgan");
 
 const {conn} = require('./db.js');
-const server = require('./microservices/contacts.js');
+const server = require('./microservices/email.js');
 
 
 conn.sync({ force: false })
 .then(() => {
   server.listen(process.env.PORT || 8000, () => {
-    console.log(`contacts microservice running on ${process.env.PORT || 8000}`);
+    console.log(`email microservice running on ${process.env.PORT || 8000}`);
   });
   console.log(`Connected to Database ${conn.config.database}, with user '${conn.config.username}' on port ${conn.config.port}`)
 })
